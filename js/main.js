ff3ec368616f5a03df9ea35980df3b379ca2df15
Constance Weightman <constance.weightman@gmail.com>
2023-06-28 22:33:26 -0700

    updated the fetch call, added a fix for missing PCTGO codes

--- js/main.js
+++ js/main.js
@@ -6,47 +6,39 @@ let searchResultList = document.querySelector('#cardResults');
 let deckList = document.querySelector('#deckList');
 
 let searchResult = [];
+let currentDeck = [];
 
 
-let emptyMessage = document.createElement('span')
-emptyMessage.innerText = 'No results found'
+let emptyMessage = document.createElement('span');
+emptyMessage.innerText = 'No results found';
 
 
 //This function takes the user input from the search bar and makes a fetch request with it. The resulting array is saved to the searchResult variable
 
-function getCards(){
+async function getCards(){
   let searchInput = '"' + document.querySelector('input').value + '"'
   
-  fetch(`https://api.pokemontcg.io/v2/cards?q=name:${searchInput}`)
-    .then(response => response.json())
-    .then(response => {
-      //for testing
-      console.log(response)
-
-      searchResult = response.data
-      console.log(searchResult)
-
-      displaySearchResults();
-    })
-    .catch(err => console.log(`error : ${err}`));
-
-}
-
-//This function clears the previously displayed searchResults. It then creates an li with an image and array id for earch card in the new search results, then appends the li to the searchResultList ul
-function displaySearchResults(){
-  clearList(searchResultList);
-  if(searchResult.length == 0){
-    searchResultList.appendChild(emptyMessage)
-  }
-  for(let i = 0; i < searchResult.length; i++){
-    searchResultList.appendChild(createSearchResult(searchResult[i], i))
-  }
+  const res = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${searchInput}`);
+  const result = await res.json();
+  searchResult = result.data;
+  displaySearchResults();
 }
 
 //This function clears all children of the passed ul
 function clearList(ul){
   while(ul.firstChild ){
     ul.removeChild(ul.firstChild);
+  }
+}
+
+//This function clears the previously displayed searchResults. It then creates an li with an image and array id for earch card in the new search results, then appends the li to the searchResultList ul
+function displaySearchResults(){
+  clearList(searchResultList);
+  if(searchResult.length == 0){
+    searchResultList.appendChild(emptyMessage);
+  }
+  for(let i = 0; i < searchResult.length; i++){
+    searchResultList.appendChild(createSearchResult(searchResult[i], i));
   }
 }
 
@@ -59,5 +51,61 @@ function createSearchResult(card, arrayIndex){
   img.src = card.images.small;
   li.appendChild(img);
 
+  li.addEventListener('click', addToDeck);
   return li;
+}
+
+//This function takes the id from the clicked card and uses it to pull the object from the searchResult array. The info is used to add the card to the deckList
+function addToDeck(){
+  let card = getCardInfo(searchResult[this.id]);
+  console.log(card);
+
+}
+
+function getCardInfo(card){
+  console.log(card);
+  let cardCode = '';
+  let setId = card.set.id;
+  //check for pctgo code
+  switch (setId){
+    case 'sv1':
+      cardCode = 'SVI';
+      break;
+    case 'sv2':
+      cardCode = 'PAL';
+      break;
+    case 'svp':
+      cardCode = 'PR-SV';
+      break;
+    default:
+      cardCode = card.set.ptcgoCode;
+  }
+
+  let createdCard = new Card(card, cardCode);
+  
+  return createdCard;
+}
+
+class Card {
+  constructor(cardInfo, cardCode){
+    this._name = `${cardInfo.name} ${cardCode} ${cardInfo.number}`;
+    this._qty = 1;
+    this._cardInfo = cardInfo;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get quantity() {
+    return this._qty;
+  }
+  
+  addOne(){
+    this._qty += 1;
+  }
+
+  subtractOne(){
+    this._qty -= 1;
+  }
 }
